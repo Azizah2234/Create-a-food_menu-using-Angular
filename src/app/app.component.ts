16cfbae95db4e68576942fbeb8708c9dc46a460c
safcsp <zoozah2234@gmail.com>
2024-06-11 04:07:45 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+ 
+  total=0;
+  qtr=0;
+  menunam:string="";
+  foodmenu=[
+    {id:1,name:"برجر",price:20,description:" برجر عادي",img:"/assets/br.png"},
+    {id:2,name:"تاكو",price:23,description:"تاكو دجاج ",img:"/assets/ta.png"},
+    {id:3,name:"شاورما",price:17,description:"شاورما عربي",img:"assets/sh.png"},
+    {id:4,name:"بيتزا",price:35,description:"بيتزا مارجريتا",img:"/assets/pz.png"}
+  ]
+  sum(event:any,price:number)
+  {
+    const ischecked:boolean=event.target.checked;
+    if(ischecked)
+    {
+      this.total=this.total+price;
+      this.qtr+=1;
+    }
+    else {
+    this.total=this.total-price;
+      this.qtr-=1;
+    }
+  }
+  find(abc:any[]){
+    return abc.filter(item => item.name.toLowerCase().includes(this.menunam.toLowerCase()))
+  }
+  
   title = 'menuch';
 }
